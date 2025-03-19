e2999fcc01645137dca0709b4e83240cd41a0ec9
Doanta Ginting <doantaaloycius@gmail.com>
2025-03-19 23:03:28 +0700

    menambahkan class SimpleDataBase

--- jurnal_modul5_21104009/Program.cs
+++ jurnal_modul5_21104009/Program.cs
@@ -1,4 +1,33 @@
-﻿// Class untuk menjumlahkan 2 angka dengan tipe data yang dapat berubah karena menggunakan Generics
+class SimpleDataBase<T>
+{
+    // Inisialisasi Properties
+    private List<T> storedData;
+    private List<DateTime> inputDates;
+
+    public SimpleDataBase()
+    {
+        // Membuat List kosong saat pembuatan instance SimpleDataBase
+        storedData = new List<T>();
+        inputDates = new List<DateTime>();
+    }
+
+    // Fungsi untuk menambahkan data ke dalam list menggunakan waktu sekarang
+    public void AddNewData(T data)
+    {
+        storedData.Add(data);
+        inputDates.Add(DateTime.Now);
+    }
+
+    // Fungsi untuk menampilkan semua data
+    public void PrintAllData()
+    {
+        for (int i = 0; i < storedData.Count; i++)
+        {
+            Console.WriteLine("Data " + (i + 1) + " Berisi " + storedData[i] + " disimpan pada waktu " + inputDates[i]);
+        }
+    }
+}
+// Class untuk menjumlahkan 2 angka dengan tipe data yang dapat berubah karena menggunakan Generics
 class Penjumlahan
 {
     public T JumlahTigaAngka<T>(T a, T b, T c) where T : struct
@@ -27,5 +56,12 @@ class Program
         
         //mencetak hasil menggunakan string concatenation
         Console.WriteLine($"Hasil Penjumlahan: {hasil}");
+        
+        SimpleDataBase<long> database = new SimpleDataBase<long>();
+        database.AddNewData(angka1);
+        database.AddNewData(angka2);
+        database.AddNewData(angka3);
+        
+        database.PrintAllData();
     }
 }
